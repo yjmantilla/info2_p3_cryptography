fea74d3acf9357c16af85cfe4ad3f3117aae0dd2
yjmantilla <yjmantilla@gmail.com>
2018-04-13 19:53:06 -0500

    codigos arduino para la comunicacion sincrona y asincrona

--- sync/sync.ino
+++ sync/sync.ino
@@ -14,7 +14,7 @@ void loop()
     if(ByteLlegando == 'k')
     {
       digitalWrite(13, HIGH);
-      Serial.print("Descomponerte,Descomponerte.\nDesentrelazarte el alma.\nDesvestirte hasta el pasado.\nDesvestirte hasta los sueños.\nMorir en tu pecho.\nTransformarte en lagrima.\nDevolverte al cielo.\nDonde perteneces.\nConvertirte en sueño.\nDevorarte el alma."); // wait for a second
+      Serial.print("mensaje"); // wait for a second
       delay(1000);
       //Serial.print("k");
       digitalWrite(13, LOW);
